5507ed82689d754fb7bb746ecf27f5408a66434d
alpho <alphonsusho10s@gmail.com>
2023-07-28 12:18:10 +0100

    streamline functions

--- Simulation.py
+++ Simulation.py
@@ -4,38 +4,35 @@ from collections import deque
 from common.config import simulation_params
 from Road import Road
 
-class SimulationManager:
+class Simulation:
     """Class to manage the simulation
 
     Args:
         ts: Time step to use when running the simulation
     """
-    def __init__(self, record_flag, pause_flag, terminate_flag):
+    def __init__(self):
         self.ts = simulation_params['ts']
         self.road = Road()
 
-        # Simulation Controls
-        # Default values commented
-        self.record_flag = record_flag # False
-        self.pause_flag = pause_flag # False
-        self.terminate_flag = terminate_flag # False
-
         self.simulation_record = []
-    def timestep(self):
-        """Single timestep frame
-        """
+        self.display_vehicles = []
 
-        if not self.terminate_flag and not self.pause_flag:
+    # def timestep(self):
+    #     """Single timestep frame
+    #     """
+
+    #     if not self.terminate_flag and not self.pause_flag:
+    #         self.road.update_road(ts=self.ts)
+
+    #     return self.road.vehicle_list
+
+    def run(self, is_paused, is_recording):
+        while not is_paused:
             self.road.update_road(ts=self.ts)
-
-        return self.road.vehicle_list
-
-    def run(self):
-        while not self.terminate_flag:
-            self.road.update_road(ts=self.ts)
-            if self.record_flag:
+            self.display_vehicles.append([vehicle.vehicle_id() for vehicle in self.timestep()])
+            if is_recording:
                 # records simulation
                 self.record_simulation.append([vehicle.vehicle_id() for vehicle in self.timestep()]) # List of dict_id
 
-        return self.simulation_record
+        return self.display_vehicles, self.simulation_record
 
