f6474922d60d11f012715c5ac491d0122c287979
alpho <alphonsusho10s@gmail.com>
2023-07-29 20:35:27 +0100

    rectify vehiclelist append problem

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from Simulation import Simulation
 from Window import Window
 import json
 
+def save_data(data, filename):
+    print("Saving data, this might take a while...")
+    json.dump(data, open(filename, "w"))
+
 def main():
     # Creating instances
     simulation = Simulation()
@@ -13,9 +17,10 @@ def main():
 
     display_vehicles, record_simulation = simulation.run()
 
-    vehicle_data = json.dumps(display_vehicles)
-    with open("data/vehicle_data.json", "w") as outfile:
-        outfile.write(vehicle_data)
+    # vehicle_data = json.dumps(display_vehicles)
+    # with open("data/vehicle_data.json", "w") as outfile:
+    #     outfile.write(vehicle_data)
+    save_data(display_vehicles, "data/vehicle_data.json")
 
 if __name__ == "__main__":
     main()
