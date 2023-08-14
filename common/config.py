b111fd41e5782a65cc79f2f59df506459866460d
alpho <alphonsusho10s@gmail.com>
2023-08-14 19:58:19 +0100

    testing timer clock

--- common/config.py
+++ common/config.py
@@ -83,7 +83,7 @@ baseline = f"ACCNo_SHC{driving_params['shc_logic']}_Road{road_params['road_close
 
 simulation_params = {
     "ts": 0.2, # was 0.001 for realtime  testing: 0.2 # Ts < 0.5 same results
-    "playback_speed": 8, # realtime = 1
+    "playback_speed": 1, # realtime = 1
     "folderpath": "data",
     "filename": f"ACC{driving_params['acc_logic']}_SHC{driving_params['shc_logic']}_RoadNo_RampIn{road_params['onramp_inflow']}_VehIn{road_params['vehicle_inflow']}",
     "record": False,
